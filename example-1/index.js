57cbda560d381700bcd8a3a9b6a4dea3b9cc4b5a
Victor de la Cruz <vcg_cruz@hotmail.com>
2022-02-16 07:56:30 -0600

    add chrome, node and electorn details

--- example-1/index.js
+++ example-1/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
@@ -5,7 +6,8 @@ const createWindow = () => {
         width: 800,
         height: 600,
         nodePreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
         }
     })
 
@@ -14,4 +16,17 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     createWindow()
-  });
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
---
+++ example-1/preload.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        console.log('replace version')
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  });
